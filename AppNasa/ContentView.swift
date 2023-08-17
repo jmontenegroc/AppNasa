1441317266b9d226748b58f2307c6b119610cd24
Fausto <j.montenegroc@uniandes.edu.co>
2023-08-16 19:45:11 -0600

    final

--- AppNasa/ContentView.swift
+++ AppNasa/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var PhotoVM = PhotoViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView{
+            VStack(spacing: 16){
+                ForEach(PhotoVM.arrPhotos){ item in
+                    VStack(spacing:32){
+                        Text(item.title)
+                            .font(.title.bold())
+                            .frame(width:UIScreen.main.bounds.width-20 )
+                        
+                        AsyncImage(url:URL(string: item.url))
+                            .frame(width:UIScreen.main.bounds.width-50)
+                            .scaledToFit()
+                            .clipped()
+                    }
+                    .background(Material.regular, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+                }
+                
+            }
+            .padding()
         }
-        .padding()
+        .navigationTitle("NASA")
+        .onAppear(perform: APIget)
+        
+    }
+    
+    func APIget() {
+        Task{
+            do{
+                try await PhotoVM.getPhotosData()
+            }
+            catch{
+                print("error")
+            }
+        }
     }
 }
 
--- AppNasa/PhotoViewModel.swift
+++ AppNasa/PhotoViewModel.swift
@@ -11,14 +11,28 @@ import SwiftUI
 
 class PhotoViewModel : ObservableObject{
     @Published  var arrPhotos = [PhotoModel]()
+    
      
-    func getPhotosData(){
+    func getPhotosData() async throws{
         guard let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=10")
         else{
             print("invalid url")
             return
         }
         let urlRequest = URLRequest(url: url)
+        let (data,response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else   {
+            print("error")
+            return
+        }
+
+        let results = try JSONDecoder().decode([PhotoModel].self, from:data)
+        
+        print(results)
+        DispatchQueue.main.async{
+            self.arrPhotos = results
+        }
         
     }
 }
